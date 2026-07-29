aadf737ec3e6c934179ddbaea9c6097e8f07f449
unknown <yannovignon@gmail.com>
2026-07-29 20:47:46 +0000

    vbvbv

--- api/delete-image.js
+++ api/delete-image.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.VITE_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.VITE_CLOUDINARY_API_KEY,
+  api_secret: process.env.VITE_CLOUDINARY_API_SECRET,
 });
 
 function extractPublicIdFromUrl(url) {

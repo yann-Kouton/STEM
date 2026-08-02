6f8ed9809078a810b9c9530e92b4ec1e10433d21
unknown <yannovignon@gmail.com>
2026-08-02 21:28:17 +0000

    nbhb

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-26b8999e'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.vbtihvfurq8"
+    "revision": "0.6bbbu750ghg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
